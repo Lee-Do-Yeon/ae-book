3792b16d0e8bb3358ef0bf0c542ee8be8a12923e
이동준 <dj0505.lee@samsung.com>
2023-04-21 08:06:55 +0900

    mentoring : 깨진 부분 보수
    - 깨진 부분 보수해서 올리기
    
    Former-commit-id: 29a9617de13b59bf7eda5446d7078202f0808503

--- example/frontend/src/main.js
+++ example/frontend/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
-<<<<<<< HEAD
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -14,12 +13,4 @@ Vue.use(IconsPlugin)
 new Vue({
 	render: h => h(App),
 	axios
-=======
-
-Vue.prototype.$http = axios
-Vue.config.productionTip = false
-
-new Vue({
-	render: h => h(App),
->>>>>>> branch 'mentoring/devops/S08P31C201-433-2-spring-and-vue-js' of https://lab.ssafy.com/s08-final/S08P31C201.git
 }).$mount('#app')
